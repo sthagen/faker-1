ae10d9a47e0bf8c71fcc5a920160a18b3a6bfc44
ST-DDT <ST-DDT@gmx.de>
2024-11-05 13:16:34 +0100

    infra(unicorn): prefer-string-slice (#3247)

--- eslint.config.ts
+++ eslint.config.ts
@@ -154,13 +154,13 @@ const config: ReturnType<typeof tseslint.config> = tseslint.config(
       'unicorn/number-literal-case': 'off', // incompatible with prettier
       'unicorn/numeric-separators-style': 'off', // "magic numbers" may carry specific meaning
       'unicorn/prefer-string-raw': 'off', // The additional prefix doesn't help readability
+      'unicorn/prefer-string-slice': 'off', // string.substring is sometimes easier to use
       'unicorn/prefer-ternary': 'off', // ternaries aren't always better
 
       // TODO @Shinigami92 2023-09-23: The following rules currently conflict with our code.
       // Each rule should be checked whether it should be enabled/configured and the problems fixed, or stay disabled permanently.
       'unicorn/consistent-function-scoping': 'off',
       'unicorn/prefer-export-from': 'off',
-      'unicorn/prefer-string-slice': 'off',
       'unicorn/prevent-abbreviations': 'off',
     },
   },
